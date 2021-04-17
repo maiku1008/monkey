79b98a0a34ee1dae4eb0238d2fdff7da78f5d96e
Michael Cuffaro <michael@cuffaro.com>
2021-04-17 14:51:50 +0200

    feat: boolean evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -7,6 +7,12 @@ import (
 	"monkey/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 // Eval recursively evaluates the given ast.Node and returns
 // an object
 func Eval(node ast.Node) object.Object {
@@ -20,6 +26,8 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -31,4 +39,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
